f439dc71a0bbfe75ac8fbfbedfc6f0bb33ba3733
Bae KwonHan <darjeeling@gmail.com>
2015-11-04 08:28:55 +0900

    init

---
+++ pycon2015/create_cover_image.py
@@ -0,0 +1,44 @@
+# coding=utf-8
+import PIL
+from PIL import ImageFont
+from PIL import Image
+from PIL import ImageDraw
+
+#font = ImageFont.truetype("./SpoqaHanSans_all/SpoqaHanSans_original/Spoqa Han Sans Regular.ttf",25)
+font = ImageFont.truetype("./SpoqaHanSans_all/SpoqaHanSans_original/Spoqa Han Sans Bold.ttf",32)
+logo_img_org = Image.open('pycon_kr_2015.png')
+# 1532 x 506
+logo_img = logo_img_org.resize((766,253))
+
+image_width=1280
+image_height=720
+
+def create_cover_image(present_date, subject, speakers):
+    img=Image.new("RGBA", (1280,720),'white')
+
+    logo_left = image_width/2 -766/2
+    logo_top = (image_height-253)/2-150
+
+    img.paste(logo_img,(logo_left, logo_top), logo_img)
+
+    draw = ImageDraw.Draw(img)
+    # draw.text((0, 0),subject,(255,255,0),font=font)
+    # get text size
+    sub_w, sub_h = draw.textsize(subject, font)
+    subject_top = logo_top + 253 + 50
+    subject_left = ( image_width - sub_w ) / 2
+    print subject_top, subject_left
+    draw.text((subject_left, subject_top),subject,'black',font=font)
+    #
+    sp_w, sp_h = draw.textsize(speakers, font)
+    speaker_top = subject_top + sub_h + 20
+    speaker_left = ( image_width - sp_w ) / 2
+    draw.text((speaker_left, speaker_top),speakers,'black',font=font)
+    draw = ImageDraw.Draw(img)
+    return img
+
+
+if __name__ == '__main__':
+    img = create_cover_image('2015/05/20',u"파이콘 한국 2015 발표제목",u" 발표자1번 " )
+    img.save('test.png')
+
---
+++ pycon2015/download_font.sh
@@ -0,0 +1,3 @@
+
+wget https://github.com/spoqa/spoqa-han-sans/releases/download/1.0.0/SpoqaHanSans_all.zip
+unzip SpoqaHanSans_all.zip
